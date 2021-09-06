f07f6042ee542462d95bf08371c9d64c28dac215
Maxine <ms.l@hotmail.com>
2021-09-07 03:05:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Auto Read QR Code/Barcode (On Screen) And Input On Webpage/Text File
+
+This program uses OBS Virtual Cam plugin to read the QR Code shown on screen, it can be modified to use webcam or other external camera. The program also input the qr code on website automatically.
+
+- [Prerequisites](#prerequisites)
+- [To Do](#ToDo)
+- [Configuration options](#configuration-options)
+- [Run the script](#run-the-script)
+
+## Prerequisites  
+- Python3 ([Download](https://www.python.org/downloads/))  
+- OBS Studio ([Download](https://obsproject.com/))  
+- OBS-VirtualCam Plugin ([Download](https://obsproject.com/forum/resources/obs-virtualcam.539/))
+
+## ToDo
+- [ ] Delay Time To Start Next Scan
+
+## Configuration Options
+- **website:** The website that you want the QR code/Barcode to be key in. If you leave it empty，the QR code will be written to result.txt instead.
+
+- **screenshot:** If this option is true, the program will screenshot the webpage or text file after 10 seconds inputting the code.
+
+- **screenshot_path:** The folder that you want the screenshot to be saved in. If you leave it empty, the screenshots will save to the program folder in default. Example: C:\\Desktop
+
+- **regex_format:** The regular expression that you want the QR code/Barcode to match. If you leave it empty, there will not be a constraint of format for QR code/Barcode.
+
+- **avoid_duplicate:** If this option is true, the program will not read the current QR code/Barcode result if it is the same as the last result.
+
+<!-- - **idle_time:**  The seconds that you want the program to wait for before scanning in another QR code/Barcode. Set this option to 0 if you don't want a delay on reading the QR code/Barcode. Only integer is allowed. -->
+- **discord_webhook_url:** The Discord's webhook will update the qr code or barcode result and the screenshot of the website (if specify) in the specified Discord server. If you don't need it, leave it blank.
+
+- **discord_message:** Discord message can be a mention to the user or anything that you would like to add.
+<!-- - **auto_start_obs:** If this option is true, the program will auto start OBS Studio to read the result. Set this to false if you prefer to open it manually or you want to use other camera. -->
+
+- **camera_position:** By default, the program will use the 2nd camera that is found in the system, which should be the OBS-VirtualCam. You may adjust it if your desired camera position is not at 2.
+<!-- - **another_key:** -->
+
+- Dangerous Configuration Options: **delay_start_ie**, **delay_connect_ie**, **delay_focus_ie**, **delay_input_ie**, **delay_process_ie**, **delay_kill_ie**
+
+    The program uses Internet Explorer to input QR Code/Barcode. As we all know, Internet Explorer is slow. The following option should only be modified if you have already make changes to the program so that it doesn't use Internet Explorer to open website, or you are confident that the Internet Explorer react fast. These options are not applicable if you leave the **website** option blank.
+
+## Run the script
+
+ 1. Modify "config.json" to fit your preference
+ 2. Install dependencies:   ```pip install -r requirements.txt```
+ 3. Setup OBS Virtual Cam
+ 3. Run [auto_scan_input_code.py](auto_scan_input_code.py): `python auto_scan_input_code.py`
---
+++ auto_scan_input_code.py
@@ -0,0 +1,191 @@
+import json
+import cv2, time, os, json, re
+from pyzbar import pyzbar
+from pywinauto.application import Application
+from datetime import datetime
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+def load_config():
+    global config
+    with open('config.json', encoding='utf-8') as json_data_file:
+        config = json.load(json_data_file)
+
+def send_message_image(discord_webhook_url,img_path):
+    discord_message=config["discord_message"]
+    webhook = DiscordWebhook(url=discord_webhook_url)
+
+    #Embed image
+    with open(img_path, "rb") as f:
+        webhook.add_file(file=f.read(), filename='image.png')  
+    
+    embed=DiscordEmbed(title='QR Code/Barcode Input')
+    embed.set_image(url="attachment://image.png")
+
+    split_img_path=img_path.split("-",3)
+    embed.add_embed_field(name='Message', value=discord_message)
+    embed.add_embed_field(name='Date', value=split_img_path[0])
+    embed.add_embed_field(name='Time', value=split_img_path[1])
+    embed.add_embed_field(name='Result', value=split_img_path[2][0:-4])
+
+    webhook.add_embed(embed)
+
+    # with open(img_path, "rb") as f:
+    #     webhook.add_file(file=f.read(), filename=img_path)
+    
+    response = webhook.execute()
+    print("Response sent")
+    return
+
+def send_message(discord_webhook_url,result):
+    discord_message=config["discord_message"]
+    webhook = DiscordWebhook(url=discord_webhook_url)
+    embed=DiscordEmbed(title='QR Code/Barcode Input')
+
+    #Embed image
+    embed.add_embed_field(name='Message', value=discord_message)
+    embed.add_embed_field(name='Date', value=datetime.now().strftime("%Y%m%d"))
+    embed.add_embed_field(name='Time', value=datetime.now().strftime("%H%M%S"))
+    embed.add_embed_field(name='Result', value=result)
+
+    webhook.add_embed(embed)
+
+    # with open(img_path, "rb") as f:
+    #     webhook.add_file(file=f.read(), filename=img_path)
+    
+    response = webhook.execute()
+    print("Response sent")
+    return
+
+
+
+def read_qrcode(last_code,frame):
+    regex_format=config['regex_format']
+    qrcode = pyzbar.decode(frame)
+    if qrcode==[]:
+        qrcode_info=-1
+    else:
+        qrcode_info = qrcode[0].data.decode('utf-8')
+        
+        if(re.match(regex_format,qrcode_info)):
+            if(config['avoid_duplicate']):
+                if qrcode_info==last_code:
+                    qrcode_info=-1
+        else:
+            qrcode_info=-1
+      
+    if qrcode_info!=-1:
+        print("QR code of",qrcode_info,"detected")
+    return qrcode_info,frame
+
+
+
+def input_qrcode (qrcode):
+    delay_connect_ie=config['delay_connect_ie']
+    delay_focus_ie=config['delay_focus_ie']
+    delay_input_ie=config['delay_input_ie']
+    delay_process_ie=config['delay_process_ie']
+
+    app=Application(backend="uia").connect(title_re = ".*Internet Explorer.*")
+    time.sleep(delay_connect_ie)
+    print([w.window_text() for w in app.windows()])
+    ie =  app.window(title_re = ".*Internet Explorer.*")
+    time.sleep(delay_focus_ie)
+    ie.set_focus()
+    print("Set focus")
+    time.sleep(delay_input_ie)
+    ie.type_keys(qrcode)
+    print("Type QR Code/Result")
+    time.sleep(delay_process_ie)
+    return ie
+
+
+
+def screenshot_qrcode(ie,qrcode,path):
+    #Screenshot
+    img = ie.capture_as_image()
+
+    #Save to path
+    now = datetime.now()
+    dt = now.strftime("%Y%m%d-%H%M%S")
+    img_name=dt+'-'+qrcode+'.png'
+    img_name.replace(" ", "")
+    
+    if(path==""):
+        dir = os.path.dirname(__file__)
+        img_path=dir+img_name   
+    else:
+        img_path=path+'\\'+img_name
+    
+    img.save(img_path)
+    
+    print("Save image")
+    return img_path
+
+
+
+def main():
+
+    #To detect the camera by its position
+    camera_position = config['camera_position']
+   
+    i=camera_position-1
+    camera = cv2.VideoCapture(i)
+    ret, frame = camera.read()
+    
+    #If camera is not found, the program will use the first camera found.
+    if ret == False:
+        i=0
+        while ret == False:       
+            camera = cv2.VideoCapture(i)
+            ret, frame = camera.read()
+            i=i+1
+    print("Camera detected at Slot",i+1)
+    
+    #To read the QR code/Barcode
+    website=config['website']
+    screenshot_path=config['screenshot_path']
+    discord_webhook_url=config['discord_webhook_url']
+    delay_start_ie=config['delay_start_ie']
+    delay_kill_ie=config['delay_kill_ie']
+    last_code=-1
+    
+    while ret:
+        ret, frame = camera.read()
+        qrcode_info,frame = read_qrcode(last_code,frame)
+        
+        if qrcode_info!=-1:
+            #Internet explorer will restart every time to input
+            if website!="":
+                app=Application().start(
+                    r"c:\program files\internet explorer\iexplore.exe {}".format(website))
+                time.sleep(delay_start_ie)
+                ie=input_qrcode(qrcode_info)
+
+                if config["screenshot"]:
+                    img_path=screenshot_qrcode(ie,qrcode_info,screenshot_path)
+                    app.kill() 
+                    time.sleep(delay_kill_ie)
+                    if discord_webhook_url!="":
+                        send_message_image(discord_webhook_url,img_path)
+                else:
+                    app.kill() 
+                    time.sleep(delay_kill_ie)
+                    if discord_webhook_url!="":
+                        send_message(discord_webhook_url,qrcode_info)
+                last_code=qrcode_info
+                qrcode_info=-1
+                           
+            else:
+                file = open("result.txt","a+")#append mode
+                file.write(qrcode_info+"\n")
+                file.close()
+                if discord_webhook_url!="":
+                    send_message(discord_webhook_url,qrcode_info)
+                last_code=qrcode_info
+                qrcode_info=-1
+            
+    camera.release()
+
+if __name__ == "__main__":
+    load_config()
+    main()
